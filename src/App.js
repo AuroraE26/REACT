fcf510ceded31a5188bb2cd1306902646ff89bbd
Aurora <unefleur26@gmail.com>
2021-12-14 13:19:39 -0600

    API rick & morty

--- src/App.js
+++ src/App.js
@@ -1,58 +1,34 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-// import { useEffect, useState } from "react";
-// // Components
-import NavBar from "./components/NavBar/NavBar";
+import { useEffect, useState } from "react";
 
-// const _users = [
-// 	{
-// 		id: 1,
-// 		firstName: "Selene",
-// 		lastName: "Chavez",
-// 	},
-// 	{
-// 		id: 2,
-// 		firstName: "Omar",
-// 		lastName: "Mijangos",
-// 	},
-// 	{
-// 		id: 3,
-// 		firstName: "Luis",
-// 		lastName: "Balán",
-// 	},
-// 	{
-// 		id: 4,
-// 		firstName: "Odon",
-// 		lastName: "Balán",
-// 	},
-// ];
+// Components
+import UserName from "./components/UserName/UserName";
 
 function App() {
-	// const [users, setUsers] = useState([]);
+	const [users, setUsers] = useState([]);
+	useEffect(() => {
+		const getUsers = async () => {
+			const response = await fetch("https://rickandmortyapi.com/api/character");
+			const data= await response.json();
+			const parsedRes = data.results;
+			console.log(parsedRes);
+			setUsers(parsedRes);
+		};
 
-	// useEffect(() => {
-	// 	// await fetch()
-	// 	setTimeout(() => {
-	// 		const response = _users;
-	// 		setUsers(response);
-	// 	}, 3000);
-	// }, []);
+		getUsers();
+	}, []);
 
-	// console.log(users);
+	const dataNew = users.map((user)=>(
+		<UserName id={user.id} name={user.name} status={user.status} species={user.species} lastLoc={user.location.name} origin={user.origin.name} image={user.image}/>
+	));
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<NavBar />
-
-				{/* {users.map((user) => (
-					<div key={user.id}>
-						<h1>
-							{user.firstName} {user.lastName}
-						</h1>
-					</div>
-				))} */}
+				{/* <img src={logo} className="App-logo" alt="logo" /> */}
+				{/* <NavBar /> */}
+				{dataNew}
 			</header>
 		</div>
 	);
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,53 +1,84 @@
 import React, { useState } from "react";
-import {newUser} from "./users.js";
+
+// Components
+import NavBarItem from "../NavBarItem/NavBarItem.js";
+
+// Styles
 import "./NavBar.css";
 
+const ULItems = [
+	{
+		id: 1,
+		text: "Item 1",
+	},
+	{
+		id: 2,
+		text: "Item 2",
+	},
+	{
+		id: 3,
+		text: "Item 3",
+	},
+	{
+		id: 4,
+		text: "Item 4",
+	},
+];
+
 function NavBar() {
-	const [active, setActive] = useState(3);
+	const [active, setActive] = useState(1);
+
 	const handleClick = (itemID) => {
 		setActive(itemID);
 	};
-	const checkActive = (itemID) => (active === itemID ? "li-active" : "");
 
+	const checkActive = (itemID) => (active === itemID ? "li-active" : "");
 
 	let content = "";
 	switch (active) {
 		case 1:
-			content = newUser[active-1];
+			content = "Holi 1";
 			break;
 		case 2:
-			content = newUser[active-1];
+			content = "Holi 2";
 			break;
 		case 3:
-			content = newUser[active-1];
+			content = "Holi 3";
 			break;
 		default:
-			content = newUser[active-1];
+			content = "No holi";
 			break;
 	}
 
 	return (
 		<div>
 			<ul>
-				<li className={checkActive(1)} onClick={() => handleClick(1)}>
-					Item 1
-				</li>
-				<li className={checkActive(2)} onClick={() => handleClick(2)}>
-					Item 2
-				</li>
-				<li className={checkActive(3)} onClick={() => handleClick(3)}>
-					Item 3
-				</li>
-				<li className={checkActive(4)} onClick={() => handleClick(4)}>
-					Item 4
-				</li>
+				{ULItems.map((item) => (
+					<NavBarItem
+						key={item.id}
+						text={item.text}
+						className={checkActive(item.id)}
+						callback={() => handleClick(item.id)}
+					/>
+				))}
 			</ul>
 			<div>
 				<h1>{content}</h1>
-				
 			</div>
 		</div>
 	);
 }
 
 export default NavBar;
+
+/*
+(el item activo es el 1)
+1. El usuario da click en el item 3
+2. El click se propaga al componente NavBarItem
+3. Dentro de NavBarItem, se ejecuta el callback
+4. El callback ejecuta a handleClick (que es el que actualiza al estado)
+5. Se ejecuta el setter setActive y se genera un nueva renderización
+6. Al momento de renderizar, como el estado active, cambio de valor, el methodo
+		checkActive cambia las clases dada la iteración del map
+7. Se renderiza
+*/
