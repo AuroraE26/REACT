fd671bb26a5909c07470548577103e767694c938
Aurora <unefleur26@gmail.com>
2021-12-16 19:31:16 -0600

    a

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import Greeting from "./components/Greeting.js"
-import Calculator from "./components/Calculator.js"
+import "./App.css";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Greeting firstName="Oscar" lastName="Ruiz"/>
-        <Calculator a={5} b="{6}" op="resta"/>
-      </header>
-    </div>
-  );
+	return <div className="container">App</div>;
 }
 
 export default App;
